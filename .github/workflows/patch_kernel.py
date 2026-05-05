2a974f379c7ebaf344f95227cc2656f0944a183d
yeq <yede6669@gmail.com>
2026-05-05 10:50:05 +0800

    创建 patch_kernel.py

---
+++ .github/workflows/patch_kernel.py
@@ -0,0 +1,38 @@
+import re, os
+
+def safe_remove_werror(text):
+    lines = text.split('\n')
+    result = []
+    for line in lines:
+        tokens = line.split(' ')
+        new_tokens = []
+        for token in tokens:
+            if '$(' in token:
+                new_tokens.append(token)
+            elif re.match(r'^-Werror(=[^\s]*)?$', token):
+                pass
+            else:
+                new_tokens.append(token)
+        result.append(' '.join(new_tokens))
+    return '\n'.join(result)
+
+makefiles = []
+for root, dirs, files in os.walk('.'):
+    dirs[:] = [d for d in dirs if d != '.git']
+    for fname in files:
+        if fname in ('Makefile', 'Kbuild') or fname.endswith('.mk'):
+            makefiles.append(os.path.join(root, fname))
+
+changed = 0
+for path in makefiles:
+    try:
+        with open(path, 'r', errors='replace') as f:
+            original = f.read()
+        new = safe_remove_werror(original)
+        if new != original:
+            with open(path, 'w') as f:
+                f.write(new)
+            changed += 1
+    except Exception as e:
+        print('skip ' + path + ': ' + str(e))
+print(str(changed) + ' file(s) modified')
